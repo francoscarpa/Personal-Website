52a609bf1611b350ae0eadb7dd09f62ee20ad1a9
Franco Scarpa <scarpafranco92@hotmail.it>
2020-10-18 15:22:24 +0200

    Decorators.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static51";
+const CACHE_NAME = "static52";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/notes/", "/offline/", "/accessibility/"];
 
 self.addEventListener("install", function (event) {
