159bdfd3af7c5874f369b18114cc52a7d62187e3
Franco Scarpa <scarpafranco92@hotmail.it>
2020-10-22 20:39:42 +0200

    Made the dates in metadata bold.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static60";
+const CACHE_NAME = "static70";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/accessibility/"];
 
 self.addEventListener("install", function (event) {
