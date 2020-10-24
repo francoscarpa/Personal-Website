5737af73107c50e791970e4a270e34ac282936be
Franco Scarpa <scarpafranco92@hotmail.it>
2020-10-24 14:00:19 +0200

    Added fade-in and fade-out transition to pages. V73.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static72";
+const CACHE_NAME = "static73";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/accessibility/"];
 
 self.addEventListener("install", function (event) {
