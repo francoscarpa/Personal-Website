b94d283316da7c018406a95792287d4953f0d6a2
Franco Scarpa <francoscarpa@MacBook-Pro-di-Franco.local>
2020-11-22 12:38:54 +0100

    V95. Added article about Grammarly. Decorators.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static94";
+const CACHE_NAME = "static95";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/imgs/svg.svg", "/style.css"];
 
 self.addEventListener("install", function (event) {
