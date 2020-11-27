b1fb258cacb91e546a3e6ba1010da9d63727d5f7
Franco Scarpa <francoscarpa@MacBook-Pro-di-Franco.local>
2020-11-28 00:33:29 +0100

    V98. Added article about MacBook coating.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static97";
+const CACHE_NAME = "static98";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/imgs/svg.svg", "/style.css"];
 
 self.addEventListener("install", function (event) {
