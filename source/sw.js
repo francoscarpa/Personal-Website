1f390597c26a60ab4eba598bc6609372829c1b63
Franco Scarpa <francoscarpa@MacBook-Pro-di-Franco.local>
2020-11-28 21:04:21 +0100

    V100 Fixed Twitter link on Contact page.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static99";
+const CACHE_NAME = "static100";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/imgs/svg.svg", "/style.css"];
 
 self.addEventListener("install", function (event) {
