129f8f2de861cd40390736db8fb257190e40c4d9
Franco Scarpa <scarpafranco92@hotmail.it>
2021-01-16 18:42:27 +0100

    ▶︎ V. 103 ◀︎ Fixed the opaque background color.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static102";
+const CACHE_NAME = "static103";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/imgs/svg.svg", "/style.css"];
 
 self.addEventListener("install", function (event) {
