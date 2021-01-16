72fa4c7a908178a76b4043bba5fbffd0afb981df
Franco Scarpa <scarpafranco92@hotmail.it>
2021-01-16 14:47:31 +0100

    ▶︎ V101 ◀︎ Removed opaque background color from hero.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static100";
+const CACHE_NAME = "static101";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/imgs/svg.svg", "/style.css"];
 
 self.addEventListener("install", function (event) {
