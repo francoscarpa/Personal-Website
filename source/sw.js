ac477f5e9f01d0069bb5f18721fd5d31ea1f26cd
Franco Scarpa <scarpafranco92@hotmail.it>
2021-07-04 21:03:15 +0200

    CACHE_NAME = "static117". Cosmetics.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static116";
+const CACHE_NAME = "static117";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/style.css"];
 
 self.addEventListener("install", function (event) {
