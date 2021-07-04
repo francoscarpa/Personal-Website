0d4483784d4c0d5f75c0ec54248e13a07fc864cb
Franco Scarpa <scarpafranco92@hotmail.it>
2021-07-04 17:48:25 +0200

    CACHE_NAME = "static115". Fixed missing H2s. Improved footer. Cosmetics.

--- source/sw.js
+++ source/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static114";
+const CACHE_NAME = "static115";
 const STATIC_FILES = ["/", "/blog/", "/links/", "/about/", "/contact/", "/privacyPolicy/", "/posts/", "/offline/", "/style.css"];
 
 self.addEventListener("install", function (event) {
