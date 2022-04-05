90c235639ced8c04d11f6973a364fae86f24c6ce
Franco Scarpa <scarpafranco92@hotmail.it>
2022-04-05 22:13:04 +0200

    Varie.

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,7 @@
-const htmlmin = require("html-minifier");
-const CleanCSS = require("clean-css");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
-    if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      });
-      return minified;
-    }
-
-    return content;
-  });
-
-  eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({
-      level: {
-        2: {
-          all: true,
-          removeDuplicateRules: true,
-        },
-      },
-    }).minify(code).styles;
-  });
 
   // Uso la proprietà order nei front matter delle pagine principali per mostrarle
   // in un certo ordine all’interno del menu di navigazione
