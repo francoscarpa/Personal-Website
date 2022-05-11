58cd549a091bd9e34ab0c1054af85ec0110d457e
Franco Scarpa <scarpafranco92@hotmail.it>
2022-05-11 23:59:16 +0200

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,7 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-  // Uso la proprietà order nei front matter delle pagine principali per mostrarle
-  // in un certo ordine all’interno del menu di navigazione
-  eleventyConfig.addCollection("mainMenuPages", function (collectionApi) {
-    var x = collectionApi.getFilteredByTag("mainMenuPage");
-    var y = x.sort((a, b) => {
-      return a.data.order - b.data.order;
-    });
-    return y;
-  });
-
-  return {
-    dir: {
-      input: "source",
-    },
-    templateFormats: ["html", "liquid", "njk", "css", "png", "jpg", "jpeg", "xml", "ico", "txt", "gif", "svg", "webmanifest", "js", "ttf", "json", "pdf"],
-  };
+	return {
+		dir: {
+			input: "source",
+    	}
+  	};
 };
