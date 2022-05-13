cd103458dd1960aac953711687c4f50dd64546d9
Franco Scarpa <scarpafranco92@hotmail.it>
2022-05-13 17:27:49 +0200

    New beginning.

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("source/*.css");
+	eleventyConfig.addPassthroughCopy("source/Images/*");
+	
 	return {
 		dir: {
 			input: "source",
