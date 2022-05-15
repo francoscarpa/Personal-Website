b8684c661a503eedfe3df451567939d6b8c55b36
Franco Scarpa <scarpafranco92@hotmail.it>
2022-05-15 12:57:30 +0200

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,7 @@
+const eleventySass = require("eleventy-sass");
+
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("source/*.css");
-	eleventyConfig.addPassthroughCopy("source/Images/*");
-	
-	return {
-		dir: {
-			input: "source",
-    	}
-  	};
+   eleventyConfig.addPassthroughCopy("source/Images/*")
+   eleventyConfig.addPlugin(eleventySass);
+   return { dir: { input: "source", output: "_site" } };
 };
