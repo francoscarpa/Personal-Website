ff9d659cb363742de12a7b00663f87e63c7e70c0
Franco Scarpa <scarpafranco92@hotmail.it>
2023-10-02 23:22:44 +0200

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
-const eleventySass = require("eleventy-sass");
-
 module.exports = function (eleventyConfig) {
+   eleventyConfig.addPassthroughCopy("source/Style/*");
    eleventyConfig.addPassthroughCopy("source/Images/*");
    eleventyConfig.addPassthroughCopy("source/*.ico");
    eleventyConfig.addPassthroughCopy("source/*.png");
@@ -8,6 +7,7 @@ module.exports = function (eleventyConfig) {
    eleventyConfig.addPassthroughCopy("source/*.webmanifest");
    eleventyConfig.addPassthroughCopy("source/*.xml");
    eleventyConfig.addPassthroughCopy("source/Documents/*");
-   eleventyConfig.addPlugin(eleventySass);
+   eleventyConfig.setLayoutResolution(false);
+
    return { dir: { input: "source", output: "_site" } };
 };
